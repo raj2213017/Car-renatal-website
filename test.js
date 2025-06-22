f0bbdf23fbef033fc9587b484468027f00f8dd95
raj2213017 <144986095+raj2213017@users.noreply.github.com>
2025-06-22 22:35:58 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,45 @@
+
+const menuBtn = document.getElementById('menu-btn');
+const navLinks = document.getElementById('nav-links');
+
+menuBtn.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
+
+// Swiper Initialization
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: true,
+  spaceBetween: 20,
+  slidesPerView: 1,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+
+// ScrollReveal animation
+ScrollReveal().reveal('.section__header, .range__card, .select__card, .story__card, .footer__col', {
+  delay: 200,
+  duration: 800,
+  distance: '50px',
+  origin: 'bottom',
+  reset: true,
+});
